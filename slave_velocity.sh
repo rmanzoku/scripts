16b336cf3471c50665c2af0381178c59439f03ee
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-03-28 07:50:53 +0000

    Create slave_velocity.sh
    すべてにおいて英語が酷いが使い物にはなるのでコミット

---
+++ slave_velocity.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -eu
+
+LOGFILE="$HOME/velocity.log"
+SAMPLES=5
+
+mysql -u "$USER" -e "show slave status\G" | grep -e Seconds_Behind_Master | cut -d":" -f2 | xargs -I{} echo "$(date +"%s") {}" >> $LOGFILE
+
+OLD=$(tail -n $SAMPLES "$LOGFILE" | head -n 1)
+LATEST=$(tail -n $SAMPLES "$LOGFILE" | tail -n 1)
+
+OLD_TIME=$(echo "$OLD" | cut -d" " -f1)
+OLD_BEHIND=$(echo "$OLD" | cut -d" " -f2)
+
+LATEST_TIME=$(echo "$LATEST" | cut -d" " -f1)
+LATEST_BEHIND=$(echo "$LATEST" | cut -d" " -f2)
+
+PERIOD=$(($LATEST_TIME - $OLD_TIME))
+DISTANCE=$(($OLD_BEHIND - $LATEST_BEHIND))
+
+VELOCITY=$(($DISTANCE / $PERIOD))
+REMAIN=$(($LATEST_BEHIND / $VELOCITY))
+
+echo Decrease Seconds_Behind_Master $VELOCITY / sec
+echo Forecast $REMAIN sec
+echo Finish at $(TZ="Asia/Tokyo" date --date "$REMAIN seconds")

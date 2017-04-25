3f822334bbb2d449a940f99cdba253200270ed41
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-04-25 09:24:00 +0000

    Add cron/hosts
    describe-instancesしてhosts形式のファイルを作るやつ

---
+++ cron/hosts-private.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -u
+
+AWS_SHARED_CREDENTIALS_FILE="$HOME/.aws/credentials"
+PROFILE_PREFIX="profile "
+
+AWSCLI="/usr/local/bin/aws"
+JQ="/usr/bin/jq"
+
+CACHE_FILE="$HOME/.cache/hosts.private"
+echo "" > "$CACHE_FILE.tmp"
+
+PROFILES=$(sed -n -e "s/^\[$PROFILE_PREFIX\(.*\)\]/\1/p" < "$AWS_SHARED_CREDENTIALS_FILE")
+
+
+for p in $PROFILES
+do
+    $AWSCLI --profile "$p" ec2 describe-instances \
+	    --filters \
+	    'Name=instance-state-name,Values=running' \
+	    'Name=tag:Name,Values=*' \
+	| $JQ -r '.Reservations[].Instances[] | [.PrivateIpAddress,(.Tags[] | select(.Key == "Name") | .Value // ""),"# "+.InstanceId] | join(" ")' \
+	      >> "$CACHE_FILE.tmp"
+
+done
+
+column -t < "$CACHE_FILE.tmp" > "$CACHE_FILE"
+
+
---
+++ cron/hosts-public.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -u
+
+AWS_SHARED_CREDENTIALS_FILE="$HOME/.aws/credentials"
+PROFILE_PREFIX="profile "
+
+AWSCLI="/usr/local/bin/aws"
+JQ="/usr/bin/jq"
+
+CACHE_FILE="$HOME/.cache/hosts.public"
+echo "" > "$CACHE_FILE.tmp"
+
+PROFILES=$(sed -n -e "s/^\[$PROFILE_PREFIX\(.*\)\]/\1/p" < "$AWS_SHARED_CREDENTIALS_FILE")
+
+
+for p in $PROFILES
+do
+    $AWSCLI --profile "$p" ec2 describe-instances \
+	    --filters \
+	    'Name=instance-state-name,Values=running' \
+	    'Name=tag:Name,Values=*' \
+	    'Name=ip-address,Values=*' \
+	| $JQ -r '.Reservations[].Instances[] | [.PublicIpAddress,(.Tags[] | select(.Key == "Name") | .Value // ""),"# "+.InstanceId] | join(" ")' \
+	      >> "$CACHE_FILE.tmp"
+
+done
+
+column -t < "$CACHE_FILE.tmp" > "$CACHE_FILE"
+
+

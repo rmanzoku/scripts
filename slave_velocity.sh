8ff963fd4fd65944ae93daf1449d4673956d3b01
Ryo Manzoku <manzoku.ryo@gmail.com>
2017-08-21 04:13:36 +0000

    Add behind

--- slave_velocity.sh
+++ slave_velocity.sh
@@ -4,6 +4,8 @@ set -eu
 
 SAMPLES=5
 
+VLOG=/tmp/sv.log
+
 mysql -u "$USER" -e "show slave status\G" | grep -e Seconds_Behind_Master | cut -d":" -f2 | xargs -I{} echo "$(date +"%s") {}" >> $VLOG
 
 OLD=$(tail -n $SAMPLES "$VLOG" | head -n 1)
@@ -21,6 +23,7 @@ DISTANCE=$(($OLD_BEHIND - $LATEST_BEHIND))
 VELOCITY=$(echo "$DISTANCE $PERIOD" | awk '{printf ("%f", $1/$2)}')
 REMAIN=$(echo "$LATEST_BEHIND $VELOCITY" | awk '{printf ("%d", $1/$2)}')
 
+echo Behind $LATEST_BEHIND \($(TZ="Asia/Tokyo" date --date "$LATEST_BEHIND seconds ago")\)
 echo Decrease Seconds_Behind_Master $VELOCITY / sec
 echo Forecast $REMAIN sec
 echo Finish at $(TZ="Asia/Tokyo" date --date "$REMAIN seconds")
